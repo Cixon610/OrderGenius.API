4ac95f7893099868446e520e8614bc8268c6dbce
kaitlynhung <g-kaitlynhung@gamania.com>
2023-08-07 17:03:16 +0800

    build(main): enableCors set to localhost:9000

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', process.env.CLIENT_URL],
+    origin: ['http://localhost:9000', process.env.CLIENT_URL],
   });
   await app.listen(9000);
 }
