1cfd7611451dc2b630cf087f2ba4b87f29ffd159
RyanC <ryan830807@hotmail.com>
2023-08-27 20:23:27 +0800

    feat(swagger): add swagger openapi

---
+++ README.MG.md
@@ -0,0 +1,6 @@
+# Documetation
+
+| Name | Url |
+| --- | --- |
+| Swagger UI | http://localhost:9000/api
+| Swagger JSON | http://localhost:9000/api-json
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS
   app.enableCors({
     origin: ['http://localhost:3000', process.env.CLIENT_URL],
     methods: ['GET', 'POST'],
     credentials: true,
   });
+
+  //#region Swagger
+  const config = new DocumentBuilder()
+    .setTitle('MenuGeniuse API')
+    .setDescription('MenuGeniuse API document')
+    .setVersion('1.0')
+    // .addTag('nestjs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  //#endregion
+
   await app.listen(process.env.PORT || 9000);
 }
 bootstrap();
