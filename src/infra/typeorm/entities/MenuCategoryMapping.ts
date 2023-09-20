978a8651db5d1516c60af890f6e88bf80911054a
RyanC <ryan830807@hotmail.com>
2023-09-20 20:25:15 +0800

    fix(typeorm): seperate out menumapping and menucategorymapping entities

--- src/infra/typeorm/entities/MenuCategoryMapping.ts
+++ src/infra/typeorm/entities/MenuCategoryMapping.ts
@@ -6,11 +6,11 @@ export class MenuMapping {
   @Column('uuid', { primary: true, name: 'id' })
   id: string;
 
-  @Column('uuid', { name: 'menu_id', nullable: true })
-  menuId: string | null;
-
   @Column('uuid', { name: 'menu_category_id', nullable: true })
   menuCategoryId: string | null;
+
+  @Column('uuid', { name: 'menu_item_id', nullable: true })
+  menuItemId: string | null;
 
   @CreateDateColumn()
   @Column('date', { name: 'creation_time', nullable: true })
