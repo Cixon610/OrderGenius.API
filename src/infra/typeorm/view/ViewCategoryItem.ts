98b91308dc516dc4ab8cb3e80e1432b795ae3022
RyanC <ryan830807@hotmail.com>
2023-10-01 10:25:08 +0800

    style(db view): use same viewentity expression

--- src/infra/typeorm/view/ViewCategoryItem.ts
+++ src/infra/typeorm/view/ViewCategoryItem.ts
@@ -1,63 +1,62 @@
-import { ViewEntity, DataSource, ViewColumn } from 'typeorm';
+import { ViewEntity, ViewColumn } from 'typeorm';
 
 @ViewEntity({
-  expression: (dataSource: DataSource) =>
-    dataSource
-      .createQueryBuilder()
-      .select('mc.business_id', 'business_id')
-      .addSelect('mc.id', 'category_id')
-      .addSelect('mc.name', 'category_name')
-      .addSelect('mc.description', 'category_description')
-      .addSelect('mc.picture_url', 'category_pictureurl')
-      .addSelect('mi.id', 'item_id')
-      .addSelect('mi.name', 'item_name')
-      .addSelect('mi.description', 'item_description')
-      .addSelect('mi.price', 'item_price')
-      .addSelect('mi.modification', 'item_modification')
-      .addSelect('mi.note', 'item_note')
-      .addSelect('mi.enable', 'item_enable')
-      .addSelect('mi.picture_url', 'item_pictureurl')
-      .from('menu_category', 'mc')
-      .leftJoin('menu_category_mapping', 'mcm', 'mc.id = mcm.menu_category_id')
-      .leftJoin('menu_item', 'mi', 'mi.id = mcm.menu_item_id'),
+  expression: `
+    SELECT mc.business_id AS business_id,
+      mc.id AS category_id,
+      mc.name AS category_name,
+      mc.description AS category_description,
+      mc.picture_url AS category_pictureurl,
+      mi.id AS item_id,
+      mi.name AS item_name,
+      mi.description AS item_description,
+      mi.price AS item_price,
+      mi.modification AS item_modification,
+      mi.note AS item_note,
+      mi.enable AS item_enable,
+      mi.picture_url AS item_pictureurl
+    FROM menu_category AS mc
+    LEFT JOIN menu_category_mapping AS mcm ON mc.id = mcm.menu_category_id
+    LEFT JOIN menu_item AS mi ON mi.id = mcm.menu_item_id;
+  `,
 })
 export class ViewCategoryItem {
-  @ViewColumn({name:'business_id'})
+  @ViewColumn({ name: 'business_id' })
   businessId: string;
 
-  @ViewColumn({name:'category_id'})
+  @ViewColumn({ name: 'category_id' })
   categoryId: string;
 
-  @ViewColumn({name:'category_name'})
+  @ViewColumn({ name: 'category_name' })
   categoryName: string;
 
-  @ViewColumn({name:'category_description'})
+  @ViewColumn({ name: 'category_description' })
   categoryDescription: string;
 
-  @ViewColumn({name:'category_pictureurl'})
+  @ViewColumn({ name: 'category_pictureurl' })
   categoryPictureUrl: string;
 
-  @ViewColumn({name:'item_id'})
+  @ViewColumn({ name: 'item_id' })
   itemId: string;
 
-  @ViewColumn({name:'item_name'})
+  @ViewColumn({ name: 'item_name' })
   itemName: string;
 
-  @ViewColumn({name:'item_description'})
+  @ViewColumn({ name: 'item_description' })
   itemDescription: string;
 
-  @ViewColumn({name:'item_price'})
+  @ViewColumn({ name: 'item_price' })
   itemPrice: number;
 
-  @ViewColumn({name:'item_modification'})
+  @ViewColumn({ name: 'item_modification' })
   itemModification: string;
 
-  @ViewColumn({name:'item_note'})
+  @ViewColumn({ name: 'item_note' })
   itemNote: string;
-  
-  @ViewColumn({name:'item_enable'})
+
+  @ViewColumn({ name: 'item_enable' })
   itemEnable: boolean;
 
-  @ViewColumn({name:'item_pictureurl'})
+  @ViewColumn({ name: 'item_pictureurl' })
   itemPictureUrl: string;
 }
