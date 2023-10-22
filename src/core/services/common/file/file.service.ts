3fa18a43cd00102499ee61cb1e25509109404047
RyanC <ryan830807@hotmail.com>
2023-10-22 23:34:54 +0800

    test s3 client

--- src/core/services/common/file/file.service.ts
+++ src/core/services/common/file/file.service.ts
@@ -1,28 +1,44 @@
 import { Injectable } from '@nestjs/common';
-import { S3 } from 'aws-sdk';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { SysConfigService } from 'src/infra/services';
 
 @Injectable()
 export class FileService {
   constructor(private readonly sysConfigService: SysConfigService) {}
   async uploadToS3(file: Express.Multer.File): Promise<any> {
-    const s3 = new S3({
-      accessKeyId: this.sysConfigService.infra.awsS3AcessKeyId,
-      secretAccessKey: this.sysConfigService.infra.AwsS3SecretAcessKey,
+    // const s3 = new S3({
+    //   accessKeyId: this.sysConfigService.infra.awsS3AcessKeyId,
+    //   secretAccessKey: this.sysConfigService.infra.AwsS3SecretAcessKey,
+    // });
+
+    const s3Client = new S3Client({
+      region: this.sysConfigService.infra.awsS3Region,
+      credentials: {
+        accessKeyId: this.sysConfigService.infra.awsS3AcessKeyId,
+        secretAccessKey: this.sysConfigService.infra.AwsS3SecretAcessKey,
+      },
     });
 
-    const params = {
-      Bucket: this.sysConfigService.infra.AwsS3BuketNameImg,
-      Key: file.originalname,
-      Body: file.buffer,
-      ACL: 'public-read',
-      ContentType: file.mimetype,
-    };
+    // const params = {
+    //   Bucket: this.sysConfigService.infra.AwsS3BuketNameImg,
+    //   Key: file.originalname,
+    //   Body: file.buffer,
+    //   ACL: 'public-read',
+    //   ContentType: file.mimetype,
+    // };
 
     try {
-      const data = await s3.upload(params).promise();
-      console.log('Bucket Created Successfully', data.Location);
-      return data.Location;
+      const data = await s3Client.send(
+        new PutObjectCommand({
+          Bucket: this.sysConfigService.infra.AwsS3BuketNameImg,
+          Key: file.originalname,
+          Body: file.buffer,
+        })
+      );
+      // const data = await s3.upload(params).promise();
+      // console.log('Bucket Created Successfully', data.Location);
+      // return data.Location;
+      return data;
     } catch (err) {
       console.log(err, err.stack);
       throw err;
