028a5be497757cd0f19351822d763fa4d8b4bc38
RyanC <ryan830807@hotmail.com>
2024-01-28 09:41:52 +0800

    fix(clientUser): lineId change to clientId

--- src/application/controllers/2c/client.user/client.user.controller.ts
+++ src/application/controllers/2c/client.user/client.user.controller.ts
@@ -3,19 +3,24 @@ import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { RoleGuard } from 'src/core/guards/role/role.guard';
 import { LineProfileVo } from 'src/core/models';
-import { LineService } from 'src/core/services';
+import { ClientUserService, LineService } from 'src/core/services';
 
-@UseGuards(AuthGuard('jwt'), RoleGuard)
+// @UseGuards(AuthGuard('jwt'), RoleGuard)
 @ApiTags('clientUser')
 @Controller('clientUser')
 export class ClientUserController {
-    constructor(private readonly lineService: LineService) {}
+  constructor(
+    private readonly lineService: LineService,
+    private readonly clientUserService: ClientUserService,
+  ) {}
 
-    @Get()
-    @ApiBearerAuth()
-    @ApiResponse({ status: 200, type: LineProfileVo })
-    async Get(@Query('lineId') id: string, @Res() res) {
-      const vo = await this.lineService.findLineAccountByLineId(id);
-      res.json(vo);
-    }
+  @Get()
+  // @ApiBearerAuth()
+  @ApiResponse({ status: 200, type: LineProfileVo })
+  async Get(@Query('clientUserId') id: string, @Res() res) {
+    //TODO: 如果id不是uuid return 400
+    const clientUser = await this.clientUserService.get(id);
+    const vo = await this.lineService.findLineAccountByLineId(clientUser.account);
+    res.json(vo);
+  }
 }
