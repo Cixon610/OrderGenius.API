606fdeeaf51992804be041e32dbbe1ecee2a1ac3
RyanC <ryan830807@hotmail.com>
2024-01-30 22:22:32 +0800

    fix(order): order detail example

--- src/core/models/vo/2c/res/order/order.res.vo/order.res.vo.ts
+++ src/core/models/vo/2c/res/order/order.res.vo/order.res.vo.ts
@@ -12,7 +12,7 @@ export class OrderResVo extends PartialType(OrderVo) {
 
   @ApiProperty({
     example:
-      '[{"itemId":"0212f053-7d09-46a4-8153-fe9013dcda10","itemName":"養樂多綠(M)","itemDescription":"","itemPrice":"45","itemPictureUrl":"","totalPrice":"145","modification":{"id":0,"name":"name","pricing":true,"options":{"key":100},"max_choices":1},"memo":"string"}]',
+      '[{"itemId":"0212f053-7d09-46a4-8153-fe9013dcda10","itemName":"養樂多綠(M)","itemDescription":"","itemPrice":"45","itemPictureUrl":"","totalPrice":"145","modification":{"id":"430ff65f-fe32-4b0b-add0-35aa1f3cb516","businessId":"7709e3c4-57bc-11ee-8c99-0242ac120002","name":"加料","options":[{"布丁":10,"仙草":5}],"maxChoices":2},"memo":"string"}]',
     description: 'detail',
   })
   detail: OrderDetailVo[] | null;
