66c07391c120645783e7f6bb5feb70dfb1bed36b
Ryan Chang <cixon610@gmail.com>
2024-01-30 01:35:16 +0000

    refactor(assistant): alter prompt flow_1

--- src/core/services/ai/openai/openai.service.ts
+++ src/core/services/ai/openai/openai.service.ts
@@ -3,8 +3,13 @@ import { Injectable } from '@nestjs/common';
 import { SysConfigService } from 'src/infra/services';
 import { delay } from 'rxjs';
 import { BusinessService } from '../../bu/business/business/business.service';
-import { ChatCreateResVo, ChatSendResVo } from 'src/core/models';
+import {
+  ChatCreateResVo,
+  ChatSendResVo,
+  ClientUserResVo,
+} from 'src/core/models';
 import { ClientUserService } from '../../bu/client/client.user/client.user.service';
+import { OrderService } from '../../bu/client/order/order.service';
 
 @Injectable()
 export class OpenaiService {
@@ -13,6 +18,7 @@ export class OpenaiService {
     private readonly SysConfigService: SysConfigService,
     private readonly businessService: BusinessService,
     private readonly clientUserService: ClientUserService,
+    private readonly orderService: OrderService,
   ) {
     this.openai = new OpenAI({
       apiKey: SysConfigService.thirdParty.opeanaiApiKey,
@@ -21,83 +27,105 @@ export class OpenaiService {
 
   async createChat(businessId: string, userId: string) {
     const business = await this.businessService.get(businessId);
-    const user = await this.clientUserService.get(userId);
-    const orderHistory = ['漢堡', '薯條', '可樂'];
-    const assistantList = await this.openai.beta.assistants.list();
-    //TODO: Assistant改成ID by business
-    const businessAssistant = assistantList.data.filter(
-      (x) => x.name === `${business.name}_${user.userName}`,
-    );
+    const prompt = await this.#getSystemPrompt(userId, business.name);
+    const assistant = await this.#getAssistant(businessId, business.name, prompt);
+
     //TODO: Thread才by user
     const thread = await this.openai.beta.threads.create();
-    if (businessAssistant.length > 0) {
-      return new ChatCreateResVo({
-        threadId: thread.id,
-        assistantId: businessAssistant[0].id,
-      });
-    }
-    //TODO: 餐單更新問題、客戶點餐歷史紀錄問題
-    //客人資訊改道send再塞?
-    const assistant = await this.openai.beta.assistants.create({
-      name: `${business.name}_${user.userName}`,
-      instructions: `你是${business.name}店員，請根據下述的各項目執行你的工作
-        # 工作目標
-        1. 請一步步引導客戶點選餐點直到能完成以下JSON的欄位
-        {
-          cutomer_name: {cutomer's name},
-          price: {total price}
-          products:[
-            {
-              Id: {index},
-              productId: {product id from the menu},
-              quantity: {product quantity},
-              modification: {comma-seperated text summary from legit modifications},
-              price: {order total price with modifications}
-            }
-          ],
-          notes: {notes for this order if customer need to leave a message to the restaurant}
-        }
-        2. 客戶點餐完畢後須再次與客戶確認品項及數量，並回報總價後再向客戶做最後確認
-        3. 客戶最終確認後，請呼叫get_weather方法，並將回傳json回應給客戶
-
-        # 工作準則
-        1. 僅能為客戶點選及推薦菜單中有的餐點
-        2. 因客戶僅能透過你點餐，如果有回答錯誤或是使客戶混淆會導致你我都失業，請確實完成工作
-        3. 如果客戶不知道點什麼，請依據以下順序規則推薦客戶餐點
-          a.如果點餐歷史紀錄有值推薦菜單內類似餐點
-          b.如果點餐歷史紀錄沒值或找不到菜單內類似餐點，請推薦菜單內promoted餐點
-
-        # 客人資訊
-        1. 客人姓名: ${user.userName}
-        2. 客人電話: ${user.phone}
-        3. 客人地址: ${user.address}
-        4. 客人點餐歷史紀錄: ${orderHistory.join(', ')}
-
-        # ${business.name}菜單
-          找好茶	M	L
-          茉莉綠茶	$30	$35
-          阿薩姆紅茶	$30	$35
-          四季春青茶	$30	$35
-          黃金烏龍	$30	$35
-          檸檬綠	$45	$55
-          梅の綠	$45	$55
-          桔子綠	$45	$55
-          8冰綠	$45	$55
-          養樂多綠	$45	$55
-          冰淇淋紅茶	$45	$55
-          旺來紅季節限定	$45	$55
-          柚子紅	$45	$55
-          鮮柚綠	$55	$65
-`,
-      tools: [{ type: 'code_interpreter' }],
-      // model: 'gpt-3.5-turbo',
-      model: 'gpt-4',
-    });
 
     return new ChatCreateResVo({
       threadId: thread.id,
       assistantId: assistant.id,
     });
+  }
+
+  //目前assistant api message role只支援user，暫時不能以system身分再疊加systemPrompt，先改用一個business_user一個Assistant的方式實作
+  //TODO:建assistant response class
+  async #getAssistant(businessId: string, name: string, systemPrompt: string) {
+    const assistantList = await this.openai.beta.assistants.list();
+    const assistantName = `${name}_${businessId}`;
+    let businessAssistant = assistantList.data.filter(
+      (x) => x.name === assistantName,
+    );
+    if (businessAssistant.length > 0) {
+      //TODO:存在則更新sysPrompt
+      await this.openai.beta.assistants.modify();
+      return businessAssistant;
+    }
+    else{
+      //不存在則建立
+      businessAssistant = await this.openai.beta.assistants.create({
+        name: assistantName,
+        instructions: systemPrompt,
+        tools: [{ type: 'code_interpreter' }],
+        model: 'gpt-4-1106-preview',
+      });
+    }
+    return businessAssistant;
+  }
+
+  async #getSystemPrompt(
+    userId: string,
+    businessName: string,
+  ): Promise<string> {
+    const user = await this.clientUserService.get(userId);
+    const orders = await this.orderService.getByUserId(userId);
+    //Get latest 10 ordered item names
+    const orderHistory = orders
+      .slice(0, 10)
+      .flatMap((x) => x.detail.map((y) => y.itemName));
+
+    const systemPrompt = `
+    你是${businessName}店員，請根據下述的各項目執行你的工作
+    # 工作目標
+    1. 請一步步引導客戶點選餐點直到能完成以下JSON的欄位
+    {
+      "detail": [
+        {
+          "itemId": "string",
+          "modifications": "",
+          "count": 0,
+          "memo": "string"
+        }
+      ],
+      "memo": "string"
+    }
+    2. 客戶點餐完畢後須再次與客戶確認品項及數量，並回報總價後再向客戶做最後確認
+    3. 客戶最終確認後，請呼叫get_weather方法，並將回傳json回應給客戶
+
+    # 工作準則
+    1. 僅能為客戶點選及推薦菜單中有的餐點
+    2. 因客戶僅能透過你點餐，如果有回答錯誤或是使客戶混淆會導致你我都失業，請確實完成工作
+    3. 如果客戶不知道點什麼，請依據以下順序規則推薦客戶餐點
+      a.如果點餐歷史紀錄有值推薦菜單內類似餐點
+      b.如果點餐歷史紀錄沒值或找不到菜單內類似餐點，請推薦菜單內promoted餐點`;
+
+    const costumerPrompt = `
+    # 客人資訊
+    1. 客人姓名: ${user.userName}
+    2. 客人電話: ${user.phone}
+    3. 客人地址: ${user.address}
+    4. 客人點餐歷史紀錄: ${orderHistory.join(', ')}`;
+
+    //TODO:組出菜單資訊
+    const menuPrompt = `
+    # ${businessName}菜單
+    找好茶	M	L
+    茉莉綠茶	$30	$35
+    阿薩姆紅茶	$30	$35
+    四季春青茶	$30	$35
+    黃金烏龍	$30	$35
+    檸檬綠	$45	$55
+    梅の綠	$45	$55
+    桔子綠	$45	$55
+    8冰綠	$45	$55
+    養樂多綠	$45	$55
+    冰淇淋紅茶	$45	$55
+    旺來紅季節限定	$45	$55
+    柚子紅	$45	$55
+    鮮柚綠	$55	$65
+    `;
+    return `${systemPrompt} ${costumerPrompt} ${menuPrompt}`;
   }
 
   async sendChat(assistantId: string, threadId: string, content: string) {
@@ -128,31 +156,33 @@ export class OpenaiService {
       delay(300);
     }
     const messages = await this.openai.beta.threads.messages.list(threadId);
-    return new ChatSendResVo({ message: messages.data[0].content[0].text.value});
+    return new ChatSendResVo({
+      message: messages.data[0].content[0].text.value,
+    });
   }
 
-  async assistantTest() {
-    const assistant = await this.openai.beta.assistants.create({
-      name: 'Math Tutor',
-      instructions:
-        'You are a personal math tutor. Write and run code to answer math questions.',
-      tools: [{ type: 'code_interpreter' }],
-      model: 'gpt-4-1106-preview',
-    });
-    const thread = await this.openai.beta.threads.create();
-    const message = await this.openai.beta.threads.messages.create(thread.id, {
-      role: 'user',
-      content: 'Can you translate "How do you turn this on" to Japanese?',
-    });
-    const run = await this.openai.beta.threads.runs.create(thread.id, {
-      assistant_id: assistant.id,
-      // instructions:
-      //   'Please address the user as Jane Doe. The user has a premium account.',
-    });
-    const runStatus = await this.openai.beta.threads.runs.retrieve(
-      thread.id,
-      run.id,
-    );
-    const messages = await this.openai.beta.threads.messages.list(thread.id);
-  }
+  // async assistantTest() {
+  //   const assistant = await this.openai.beta.assistants.create({
+  //     name: 'Math Tutor',
+  //     instructions:
+  //       'You are a personal math tutor. Write and run code to answer math questions.',
+  //     tools: [{ type: 'code_interpreter' }],
+  //     model: 'gpt-4-1106-preview',
+  //   });
+  //   const thread = await this.openai.beta.threads.create();
+  //   const message = await this.openai.beta.threads.messages.create(thread.id, {
+  //     role: 'user',
+  //     content: 'Can you translate "How do you turn this on" to Japanese?',
+  //   });
+  //   const run = await this.openai.beta.threads.runs.create(thread.id, {
+  //     assistant_id: assistant.id,
+  //     // instructions:
+  //     //   'Please address the user as Jane Doe. The user has a premium account.',
+  //   });
+  //   const runStatus = await this.openai.beta.threads.runs.retrieve(
+  //     thread.id,
+  //     run.id,
+  //   );
+  //   const messages = await this.openai.beta.threads.messages.list(thread.id);
+  // }
 }
