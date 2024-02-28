dd95ef97e5965934399d551cb71d2dbfa680ab48
RyanC <ryan830807@hotmail.com>
2024-02-28 20:26:52 +0800

    refactor(githubService): due to octokit cannot build in render, use axios to get github api

--- src/infra/services/github/github.service.ts
+++ src/infra/services/github/github.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Octokit } from '@octokit/core';
 import { SysConfigService } from '../config/sys.config.service';
+import axios from 'axios';
 
 @Injectable()
 export class GithubService {
@@ -19,15 +20,34 @@ export class GithubService {
     return await this.#getPromptContent('function.calling.json');
   }
 
+  //get content from github with axios
   async #getPromptContent(fileName: string) {
-    const response = await this.octokit.request(
-      'GET /repos/{owner}/{repo}/contents/{path}',
-      {
-        owner: 'OrderGPT',
-        repo: 'Prompts',
-        path: fileName,
-      },
-    );
-    return Buffer.from(response.data['content'], 'base64').toString('utf8');
+    try {
+      const response = await axios.get(
+        `https://api.github.com/repos/OrderGPT/Prompts/contents/${fileName}`,
+        {
+          headers: {
+            Authorization: `Bearer ${this.sysConfigService.thirdParty.githubToken}`,
+          },
+        },
+      );
+
+      return Buffer.from(response.data['content'], 'base64').toString('utf8');
+    } catch (error) {
+      console.error(error);
+    }
   }
+
+  // get content from github with Octokit
+  // async #getPromptContent(fileName: string) {
+  //   const response = await this.octokit.request(
+  //     'GET /repos/{owner}/{repo}/contents/{path}',
+  //     {
+  //       owner: 'OrderGPT',
+  //       repo: 'Prompts',
+  //       path: fileName,
+  //     },
+  //   );
+  //   return Buffer.from(response.data['content'], 'base64').toString('utf8');
+  // }
 }
